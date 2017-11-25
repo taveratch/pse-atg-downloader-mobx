e0ef7b58047e4c5bc2eac8bae8264e47db967fcf
Taweerat Chaiman <taweesoft@gmail.com>
2017-11-25 10:45:58 +0700

    add error message

---
+++ src/common/components/ErrorMessage/index.js
@@ -0,0 +1,8 @@
+import { colors, spaces } from 'src/common/mixins'
+
+import styled from 'styled-components'
+
+export default styled.div`
+  color: ${colors.red};
+  margin-bottom: ${spaces.normal};
+`
--- src/stores/fetched-store.js
+++ src/stores/fetched-store.js
@@ -1,18 +1,24 @@
 import { action, observable } from 'mobx'
 
 class FetchedStore {
-    @observable fetching = false
-    @observable success = true
+  @observable fetching = false
+  @observable success = true
+  @observable message = ''
 
-    @action.bound
-    setFetching(fetching) {
-      this.fetching = fetching
-    }
+  @action.bound
+  _setFetching(fetching) {
+    this.fetching = fetching
+  }
 
-    @action.bound
-    setSuccess(success) {
-      this.success = success
-    }
+  @action.bound
+  _setSuccess(success) {
+    this.success = success
+  }
+
+  @action.bound
+  _setMessage(message) {
+    this.message = message
+  }
 }
 
 export default FetchedStore
