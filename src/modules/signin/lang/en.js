76e21a39f76d018de915d54565fd750582cd5e87
Taweerat Chaiman <taweesoft@gmail.com>
2017-12-06 21:28:04 +0700

    add translation for signin header

--- src/modules/signin/lang/en.js
+++ src/modules/signin/lang/en.js
@@ -1,5 +1,6 @@
 export default {
   'signin.signin': 'Sign in',
   'signin.email': 'E-mail',
-  'signin.password': 'Password'
+  'signin.password': 'Password',
+  'signin.header': 'ATG Inventory Downloader'
 }
